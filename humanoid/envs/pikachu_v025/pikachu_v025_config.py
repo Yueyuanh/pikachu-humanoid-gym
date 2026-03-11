361ccc0d67942c5c29031dfec452f60889240b96
Yueyuanh <3117144922@qq.com>
2026-03-11 16:37:29 +0800

    djp=0.1

--- humanoid/envs/pikachu_v025/pikachu_v025_config.py
+++ humanoid/envs/pikachu_v025/pikachu_v025_config.py
@@ -235,7 +235,7 @@ class PikachuCfg(LeggedRobotCfg):
             low_speed = 0.2 #0.2
             track_vel_hard = 0.5 #0.5
             # base pos
-            default_joint_pos = 0.05
+            default_joint_pos = 0.1
             orientation = 1.
             base_height = 0.2
             base_acc = 0.2
