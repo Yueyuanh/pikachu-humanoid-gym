bdba57d0938218a282fa64db351c65d16cf6acd1
Yueyuanh <3117144922@qq.com>
2026-03-12 15:31:52 +0800

    feet_clearance = 1. feet_contact_number = 1. stand_still=0.5

--- humanoid/envs/pikachu_v025/pikachu_v025_config.py
+++ humanoid/envs/pikachu_v025/pikachu_v025_config.py
@@ -216,9 +216,9 @@ class PikachuCfg(LeggedRobotCfg):
             # reference motion tracking
             joint_pos = 5
             # 抬脚高度奖励
-            feet_clearance = 1.5
+            feet_clearance = 1
             # 每只脚接触顺序
-            feet_contact_number = 1.5
+            feet_contact_number = 1
             
             # gait
             feet_air_time = 1.5
@@ -227,7 +227,7 @@ class PikachuCfg(LeggedRobotCfg):
             contact_no_vel = -1
             feet_distance = 0.2
             knee_distance = 0.2
-            stand_still = -0.8
+            stand_still = -0.5
             # contact
             feet_contact_forces = -0.01
             # vel tracking
